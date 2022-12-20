4a7ebce264416d179d05ebf7cf71b67bbdc800cc
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-12-19 20:12:49 -0500

    Added Meal Items And Ui Components For it

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,13 +4,14 @@ import React, {Fragment} from 'react';
 //style imports
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
     return(
         <Fragment>
             <header className={classes.header}>
-                <h1>BeasleyMeals</h1>
-                <button>Cart</button>
+                <h1>Friskies</h1>
+                <HeaderCartButton/>
             </header> 
             <div className={classes['main-image']}>
                 <img 
