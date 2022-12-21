7407680406f90a95d00ddc035a543fa96de4a1c8
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-12-20 21:35:58 -0500

    Finishing touches

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,25 +1,21 @@
-//native react imports
-import React, {Fragment} from 'react';
+import { Fragment } from 'react';
 
-//style imports
+import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
-import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
-    return(
-        <Fragment>
-            <header className={classes.header}>
-                <h1>Friskies</h1>
-                <HeaderCartButton onClick={props.onShowCart}/>
-            </header> 
-            <div className={classes['main-image']}>
-                <img 
-                src={mealsImage}
-                alt='Table with food'/>
-            </div>   
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
 
 export default Header;
