78566b7fcc21f0b6b0cec5da9c9811fe336d29ae
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2023-02-12 17:16:18 -0500

    Added React Icons To the project  and added a icon

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-
+import {FaPaw} from 'react-icons/fa';
 import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
@@ -8,7 +8,14 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
+        <div className={classes.headerIcon}>
+        <FaPaw 
+          style={{
+            fontSize:"1.8rem"
+          }}
+        />
         <h1>Friskies</h1>
+        </div>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
